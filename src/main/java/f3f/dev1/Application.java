723436bc5e5df9a262864d2767bd22a680bd96f8
Owen-Choi <oldstyle482@gmail.com>
2022-12-03 15:59:19 +0900

    [ADD] 설정정보

--- src/main/java/f3f/dev1/Application.java
+++ src/main/java/f3f/dev1/Application.java
@@ -1,5 +1,11 @@
 package f3f.dev1;
 
-public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
