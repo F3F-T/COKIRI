b872817367278e08a3782a06393337838d8dfbf4
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-12-11 16:28:20 +0900

    [ADD] 유저 서비스에 대한 기본적인 테스트 작성, 구체적인 값으로 테스트하게 리팩터링 예정, 유저 업데이트, 삭제 테스트 추가 예정

--- src/test/java/f3f/dev1/user/UserServiceTest.java
+++ src/test/java/f3f/dev1/user/UserServiceTest.java
@@ -4,18 +4,27 @@ import f3f.dev1.domain.model.Address;
 import f3f.dev1.domain.scrap.dao.ScrapRepository;
 import f3f.dev1.domain.user.application.UserService;
 import f3f.dev1.domain.user.dao.UserRepository;
-import f3f.dev1.domain.user.dto.UserDTO;
+import f3f.dev1.domain.user.dto.UserDTO.LoginRequest;
+import f3f.dev1.domain.user.dto.UserDTO.UpdateUserInfo;
+import f3f.dev1.domain.user.dto.UserDTO.UserInfo;
+import f3f.dev1.domain.user.exception.DuplicateEmailException;
+import f3f.dev1.domain.user.exception.DuplicatePhoneNumberExepction;
+import f3f.dev1.domain.user.exception.InvalidPasswordException;
+import f3f.dev1.domain.user.exception.NotFoundByEmailException;
 import f3f.dev1.domain.user.model.User;
+import f3f.dev1.global.error.exception.NotFoundByIdException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.util.Optional;
+
+import static f3f.dev1.domain.user.dto.UserDTO.SignUpRequest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Transactional
 @SpringBootTest
@@ -30,11 +39,40 @@ public class UserServiceTest {
     @Autowired
     ScrapRepository scrapRepository;
 
+    // 회원가입 DTO 생성 메소드
+    public SignUpRequest createSignUpRequest() {
+        return SignUpRequest.builder()
+                .userName("username")
+                .nickname("nickname")
+                .phoneNumber("phoneNumber")
+                .email("userEmail")
+                .address(new Address())
+                .password("password")
+                .build();
+    }
+
+    // 로그인 DTO 생성 메소드
+    public LoginRequest createLoginRequest() {
+        return LoginRequest.builder()
+                .email("userEmail")
+                .password("password").build();
+    }
+
+    // 업데이트 DTO 생성 메소드
+    public UpdateUserInfo createUpdateRequest(Long userId) {
+        return UpdateUserInfo.builder()
+                .id(userId)
+                .address(new Address())
+                .nickname("newNickname")
+                .build();
+    }
+
+    // 회원가입 테스트
     @Test
     @DisplayName("유저 생성 성공 테스트")
-    public void signUpTestSuccess() throws Exception{
+    public void signUpTestSuccess() throws Exception {
         //given
-        UserDTO.SignUpRequest signUpRequest = new UserDTO.SignUpRequest("username", "nickname", new Address(), "phoneNumber", "email", "password");
+        SignUpRequest signUpRequest = createSignUpRequest();
 
 
         // when
@@ -43,4 +81,166 @@ public class UserServiceTest {
         // then
         assertThat(byId.get().getId()).isEqualTo(userId);
     }
+
+    @Test
+    @DisplayName("중복 이메일로 유저 생성 실패 테스트")
+    public void signUpTestFailByEmail() throws Exception {
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest();
+        Long userId = userService.signUp(signUpRequest);
+
+
+        // then
+        SignUpRequest differentRequest = SignUpRequest.builder()
+                .email("userEmail")
+                .userName("differentUser")
+                .nickname("differentUser")
+                .phoneNumber("differentUser")
+                .address(new Address())
+                .password("differentUser")
+                .build();
+
+        assertThrows(DuplicateEmailException.class, () -> userService.signUp(differentRequest));
+    }
+
+    @Test
+    @DisplayName("중복 핸드폰 번호로 유저 생성 실패 테스트")
+    public void signUpTestFailByPhoneNumber() throws Exception {
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest();
+        Long userId = userService.signUp(signUpRequest);
+
+        // when
+        SignUpRequest differentRequest = SignUpRequest.builder()
+                .email("differentUser")
+                .userName("differentUser")
+                .nickname("differentUser")
+                .phoneNumber("phoneNumber")
+                .address(new Address())
+                .password("differentUser")
+                .build();
+
+        // then
+        assertThrows(DuplicatePhoneNumberExepction.class, () -> userService.signUp(differentRequest));
+    }
+
+    // 로그인 테스트
+    @Test
+    @DisplayName("이메일, 비밀번호로 존재하는 유저 검증 성공 테스트")
+    public void loginTestSuccess() throws Exception {
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest();
+        Long userId = userService.signUp(signUpRequest);
+
+
+        // when
+        LoginRequest loginRequest = createLoginRequest();
+
+
+        // then
+        assertThat(userId).isEqualTo(userService.login(loginRequest));
+    }
+
+    @Test
+    @DisplayName("아이디 값(이메일)으로 존재하는 유저가 없어서 로그인 실패 테스트")
+    public void loginTestFailByEmail() throws Exception {
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest();
+        Long userId = userService.signUp(signUpRequest);
+
+
+        // when
+        LoginRequest differentUser = LoginRequest.builder()
+                .email("uniqueEmail")
+                .password("password").build();
+        // then
+        assertThrows(NotFoundByEmailException.class, () -> userService.login(differentUser));
+    }
+
+    @Test
+    @DisplayName("잘못된 비밀번호 값으로 로그인 실패 테스트")
+    public void loginTestFailByPassword() throws Exception {
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest();
+        Long userId = userService.signUp(signUpRequest);
+
+        // when
+        LoginRequest differentUser = LoginRequest.builder()
+                .email("userEmail")
+                .password("wrongPassword").build();
+
+        // then
+        assertThrows(InvalidPasswordException.class, () -> userService.login(differentUser));
+    }
+
+    // 유저 정보 조회 테스트
+    @Test
+    @DisplayName("유저 정보 가져오는 테스트")
+    public void getUserInfoTestSuccess() throws Exception {
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest();
+        Long userId = userService.signUp(signUpRequest);
+
+
+        // when
+
+        UserInfo userInfo = userService.getUserInfo(userId);
+
+        // then
+        assertArrayEquals(new String[]{
+                signUpRequest.getUserName(),
+                signUpRequest.getNickname(),
+                signUpRequest.getEmail(),
+                signUpRequest.getPhoneNumber()}, new String[]{
+                userInfo.getUserName(),
+                userInfo.getNickname(),
+                userInfo.getEmail(),
+                userInfo.getPhoneNumber()
+        });
+    }
+
+    @Test
+    @DisplayName("유저 정보 조회 실패 테스트 - 존재하지 않는 아이디로 요청")
+    public void getUserInfoTestFailById() throws Exception{
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest();
+        Long userId = userService.signUp(signUpRequest);
+
+
+        // then
+        assertThrows(NotFoundByIdException.class, () -> userService.getUserInfo(userId + 1));
+    }
+
+    // 유저 정보 업데이트 테스트
+    @Test
+    @DisplayName("유저 정보 업데이트 성공 테스트")
+    public void updateUserInfoTestSuccess() throws Exception{
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest();
+        Long userId = userService.signUp(signUpRequest);
+        UpdateUserInfo updateRequest = createUpdateRequest(userId);
+
+        // when
+        userService.updateUserInfo(updateRequest);
+        Optional<User> byId = userRepository.findById(userId);
+        // then
+        assertThat(updateRequest.getNickname()).isEqualTo(byId.get().getNickname());
+    }
+
+    // 유저 삭제 테스트
+    @Test
+    @DisplayName("유저 삭제 성공 테스트")
+    public void deleteUserTestSuccess() throws Exception{
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest();
+        Long userId = userService.signUp(signUpRequest);
+
+        // when
+        userService.deleteUser(userId);
+        // then
+        assertThrows(NotFoundByIdException.class, () -> userService.getUserInfo(userId));
+    }
+
+
+
 }
