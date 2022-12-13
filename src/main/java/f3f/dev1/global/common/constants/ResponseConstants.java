2410f108e1178d3e42440fd897c01cfe1bbdf4a2
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-12-13 17:29:30 +0900

    [ADD] ResponseConstants 응답형식 ResponseEntity로 변경, ExceptionHandler 생성

--- src/main/java/f3f/dev1/global/common/constants/ResponseConstants.java
+++ src/main/java/f3f/dev1/global/common/constants/ResponseConstants.java
@@ -1,10 +1,27 @@
 package f3f.dev1.global.common.constants;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class ResponseConstants {
-    public static final String OK = "OK";
-    public static final String DELETE = "DELETE";
-    public static final String CREATE = "CREATE";
-    public static final String FAIL = "FAIL";
-    public static final String UPDATE = "UPDATE";
-    public static final String COMPLETE = "OK";
+    public static final ResponseEntity<String> OK = ResponseEntity.ok("OK");
+    public static final ResponseEntity<String> DELETE = ResponseEntity.ok("DELETE");
+    public static final ResponseEntity<Void> CREATE =
+            ResponseEntity.status(HttpStatus.CREATED).build();
+    public static final ResponseEntity<Void> FAIL = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
+    public static final ResponseEntity<String> UPDATE = ResponseEntity.ok("UPDATE");
+    public static final ResponseEntity<String> COMPLETE = ResponseEntity.ok("COMPLETE");
+
+    public static final ResponseEntity<String> DUPLICATE_EMAIL = new ResponseEntity<>("이메일 중복", HttpStatus.CONFLICT);
+
+    public static final ResponseEntity<String> DUPLICATE_NICKNAME = new ResponseEntity<>("닉네임 중복", HttpStatus.CONFLICT);
+
+    public static final ResponseEntity<String> DUPLICATE_PHONENUMBER = new ResponseEntity<>("전화번호 중복", HttpStatus.CONFLICT);
+
+    public static final ResponseEntity<String> USER_NOT_FOUND =
+            new ResponseEntity<>(
+                    "ID 또는 PW를 확인하세요.", HttpStatus.NOT_FOUND
+            );
+
 }
