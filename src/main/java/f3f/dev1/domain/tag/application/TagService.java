9a4820ac86d1d0eda470579c1895c502e78955b8
Owen-Choi <oldstyle482@gmail.com>
2022-12-13 16:37:15 +0900

    [ADD] tag 기능 구현 시작

--- src/main/java/f3f/dev1/domain/tag/application/TagService.java
+++ src/main/java/f3f/dev1/domain/tag/application/TagService.java
@@ -1,4 +1,12 @@
 package f3f.dev1.domain.tag.application;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
+@RequiredArgsConstructor
 public class TagService {
+
 }
