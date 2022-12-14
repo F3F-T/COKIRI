69583e0afa25550d4e96390ec31cedf9e2e5d0e4
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-12-14 10:10:53 +0900

    [ADD] 코드리뷰를 위한 주석 추가

--- src/main/java/f3f/dev1/domain/trade/application/TradeService.java
+++ src/main/java/f3f/dev1/domain/trade/application/TradeService.java
@@ -8,7 +8,9 @@ import f3f.dev1.domain.trade.dto.TradeDTO;
 import f3f.dev1.domain.trade.dto.TradeDTO.TradeInfoDto;
 import f3f.dev1.domain.trade.exception.InvalidSellerIdException;
 import f3f.dev1.domain.trade.model.Trade;
+import f3f.dev1.domain.user.application.SessionLoginService;
 import f3f.dev1.domain.user.dao.UserRepository;
+import f3f.dev1.domain.user.exception.NotAuthorizedException;
 import f3f.dev1.domain.user.model.User;
 import f3f.dev1.global.error.exception.NotFoundByIdException;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +36,8 @@ public class TradeService {
 
     private final PostRepository postRepository;
 
+    private final SessionLoginService sessionLoginService;
+
     // 트레이드 생성 메소드
     @Transactional
     public Long createTrade(CreateTradeDto createTradeDto) {
@@ -41,6 +45,14 @@ public class TradeService {
         User seller = userRepository.findById(createTradeDto.getSellerId()).orElseThrow(NotFoundByIdException::new);
         User buyer = userRepository.findById(createTradeDto.getBuyerId()).orElseThrow(NotFoundByIdException::new);
         Post post = postRepository.findById(createTradeDto.getPostId()).orElseThrow(NotFoundByIdException::new);
+
+
+        // TODO: 세션에서 현재 로그인된 유저의 정보를 문제 없이 가져올 수 있으면 프론트에서도 seller 값을 안 넣어서 보내도 될 것 같음, 피드백 후 수정예정
+        // TODO: 현재는 로그인할때 Response도 단순히 OK로 되어있음, 유저가 요청을 보내면 백엔드에서 세션에서 해당 유저 정보를 가져올 수 있다고 생각해서 프론트에게 현재 로그인한 유저에 대한 값을 안 넘김
+        // TODO: 아래와 같은 검증이 필요하면 로그인 리턴 형식도 변경 예정
+        if (!seller.getEmail().equals(sessionLoginService.getLoginUser())) {
+            throw new NotAuthorizedException();
+        }
 
         if (!sellerId.equals(post.getAuthor().getId())) {
             throw new InvalidSellerIdException();
