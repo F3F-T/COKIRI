b7bbf26db10f219524bf98111334e5a846fdb6d7
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-12-15 19:45:31 +0900

    [ADD]CSS nav 연습

--- frontend/src/component/Nav.js
+++ frontend/src/component/Nav.js
@@ -1,33 +1,30 @@
 import React, {useState, useEffect, useMemo, useCallback} from 'react';
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import '../styles/Nav.css'
+import styles from '../styles/nav/nav.module.css'
 
 const Nav = () => {
     return (
-        <header>
-        <nav>
-            <div>
-                <NavLink to="/">
-                    Home
-                </NavLink>
-            </div>
-            <div>
-                <NavLink to="/mulmultrade">
-                    물물교환
-                </NavLink>
-            </div>
-            <div>
-                <NavLink to="/kirikiritrade">
-                    끼리끼리
-                </NavLink>
-            </div>
-            <div>
-                <NavLink to="/mypage">
-                    마이페이지
-                </NavLink>
-            </div>
-        </nav>
-        </header>
+        <div className={styles.outerNavbar}>
+            {/*<a className={styles.logo}>*/}
+            {/*    코끼리 사진*/}
+            {/*</a>*/}
+            <nav>
+                <ul>
+                    <li><NavLink to="/">Home </NavLink></li>
+                    <li><NavLink to="/mulmultrade">
+                        물물교환
+                    </NavLink></li>
+                    <li><NavLink to="/kirikiritrade">
+                        끼리끼리
+                    </NavLink></li>
+                    <li><NavLink to="/mypage">
+                        마이페이지
+                    </NavLink></li>
+                </ul>
+
+            </nav>
+        </div>
     );
 };
 
