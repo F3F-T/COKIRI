35ecd7bc97522d6acd914e5f2d69151938e9f112
Owen-Choi <oldstyle482@gmail.com>
2022-12-15 14:17:47 +0900

    [ADD] comment 서비스 피드백 반영

--- src/main/java/f3f/dev1/domain/comment/application/CommentService.java
+++ src/main/java/f3f/dev1/domain/comment/application/CommentService.java
@@ -38,7 +38,7 @@ public class CommentService {
 
     // 부모 댓글 생성 로직
     @Transactional
-    public Long createParentComment(@Valid CreateParentCommentRequest parentCommentRequest) {
+    public Long createParentComment(CreateParentCommentRequest parentCommentRequest) {
         User user = userRepository.findById(parentCommentRequest.getAuthor().getId()).orElseThrow(NotFoundByIdException::new);
         Post post = postRepository.findById(parentCommentRequest.getPost().getId()).orElseThrow(NotFoundByIdException::new);
         Comment comment = parentCommentRequest.ToParentEntity();
@@ -48,7 +48,7 @@ public class CommentService {
     }
 
     @Transactional
-    public Long createChildComment(@Valid CreateChildCommentRequest childCommentRequest) {
+    public Long createChildComment(CreateChildCommentRequest childCommentRequest) {
         User user = userRepository.findById(childCommentRequest.getAuthor().getId()).orElseThrow(NotFoundByIdException::new);
         Post post = postRepository.findById(childCommentRequest.getPost().getId()).orElseThrow(NotFoundByIdException::new);
         Comment parent = commentRepository.findById(childCommentRequest.getParent().getId()).orElseThrow(NotFoundByIdException::new);
@@ -66,26 +66,20 @@ public class CommentService {
     @Transactional(readOnly = false)
     public FindByIdCommentResponse findCommentById(Long id) {
         Comment comment = commentRepository.findById(id).orElseThrow(NotFoundByIdException::new);
-        @Valid FindByIdCommentResponse response = new FindByIdCommentResponse(comment);
+        FindByIdCommentResponse response = new FindByIdCommentResponse(comment);
         return response;
     }
 
     // post로 조회
-    // TODO Repository 명명규칙 이상 없나?
     @Transactional(readOnly = true)
     public FindByPostIdCommentListResponse findCommentsByPostId(Long postId) {
         if(!commentRepository.existsByPostId(postId)) {
             throw new NotFoundByIdException();
         }
         List<Comment> byPostId = commentRepository.findByPostId(postId);
-        @Valid FindByPostIdCommentListResponse response = new FindByPostIdCommentListResponse(byPostId);
+        FindByPostIdCommentListResponse response = new FindByPostIdCommentListResponse(byPostId);
         return response;
     }
-
-    // 게시글 주인이 쓴 댓글 조회? (작성자 태그 등을 표시해주기 위해 필요할 수도 있다고 생각함)
-    // 저자로 댓글 조회?
-    // 근데 백에서 할 일이 아닌 것 같기도 하다
-    // TODO 피드백 받기
 
     /*
         U: Update
@@ -93,7 +87,7 @@ public class CommentService {
      */
 
     @Transactional
-    public String updateComment(@Valid UpdateCommentRequest updateCommentRequest) {
+    public String updateComment(UpdateCommentRequest updateCommentRequest) {
         Post post = postRepository.findById(updateCommentRequest.getId()).orElseThrow(NotFoundByIdException::new);
         Comment comment = commentRepository.findById(updateCommentRequest.getId()).orElseThrow(NotFoundByIdException::new);
         Comment commentInPost = commentRepository.findByPostIdAndId(post.getId(), comment.getId()).orElseThrow(NotFoundByIdException::new);
@@ -105,19 +99,18 @@ public class CommentService {
     }
 
     @Transactional
-    public String deleteComment(@Valid DeleteCommentRequest deleteCommentRequest) {
+    public String deleteComment(DeleteCommentRequest deleteCommentRequest) {
         Post post = postRepository.findById(deleteCommentRequest.getId()).orElseThrow(NotFoundByIdException::new);
         User user = userRepository.findById(deleteCommentRequest.getAuthor().getId()).orElseThrow(NotFoundByIdException::new);
         Comment comment = commentRepository.findById(deleteCommentRequest.getId()).orElseThrow(NotFoundByIdException::new);
         Comment commentInPost = commentRepository.findByPostIdAndId(post.getId(), comment.getId()).orElseThrow(NotFoundByIdException::new);
-        if(commentInPost.getId() != comment.getId()) {
+        if(commentInPost.getId().equals(comment.getId())) {
             throw new NotMatchingCommentException("요청한 게시글에 수정하려는 댓글이 없습니다.");
         }
-        Comment authorComment = commentRepository.findByAuthorIdAndId(user.getId(), commentInPost.getId()).orElseThrow(NotFoundByIdException::new);
-        if(authorComment.getId() != commentInPost.getId()) {
-           throw new NotMatchingAuthorException("댓글 작성자가 아닙니다.");
+        if(commentInPost.getAuthor().getId().equals(deleteCommentRequest.getAuthor().getId())) {
+            throw new NotMatchingAuthorException("댓글 작성자가 아닙니다");
         }
-        commentRepository.delete(authorComment);
+        commentRepository.delete(commentInPost);
         return DELETE;
     }
 }
