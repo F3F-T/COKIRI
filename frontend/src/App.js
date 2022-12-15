a50d52788cbe1e4dec7bfd8844542f4de6fc0a80
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-12-15 18:16:21 +0900

    [ADD]CSS nav margin 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,16 +8,19 @@ import MulmulTrade from "./routes/MulmulTrade";
 import kiriKiriTrade from "./routes/KiriKiriTrade";
 import KiriKiriTrade from "./routes/KiriKiriTrade";
 import Nav from "./component/Nav";
+import styles from "./styles/App.module.css";
 function App() {
   return (
-      <div className="App">
-          <Nav />
+      <div>
+      <Nav />
+      <div className={styles.content}>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/mulmultrade" component={ MulmulTrade} />
           <Route path="/kirikiritrade" component={KiriKiriTrade} />
           <Route path="/mypage" component={MyPage} />
         </Switch>
+      </div>
       </div>
   );
 }
--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css"
 
 const Home = () => {
     return (<body>
-
+hi
         </body>
     );
 }
