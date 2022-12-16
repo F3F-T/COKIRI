7163eebee7b45723d6481308cb8fb260e0e9b3f2
Owen-Choi <oldstyle482@gmail.com>
2022-12-16 15:57:14 +0900

    [ADD] ScrapPostRepository 추가

--- src/test/java/f3f/dev1/post/PostServiceTest.java
+++ src/test/java/f3f/dev1/post/PostServiceTest.java
@@ -1,4 +1,62 @@
 package f3f.dev1.post;
 
+import f3f.dev1.domain.category.model.Category;
+import f3f.dev1.domain.comment.dao.CommentRepository;
+import f3f.dev1.domain.member.dao.MemberRepository;
+import f3f.dev1.domain.member.model.Member;
+import f3f.dev1.domain.post.dao.PostRepository;
+import f3f.dev1.domain.post.dto.PostDTO;
+import f3f.dev1.domain.trade.model.Trade;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static f3f.dev1.domain.post.dto.PostDTO.*;
+
+@SpringBootTest
 public class PostServiceTest {
+
+    @Autowired
+    MemberRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+
+//    public PostSaveRequest CreatePostSaveRequestDTO(String title, String content, Boolean tradeEachOther, Member author, Category productCategory, Category wishCategory, Trade trade) {
+//        PostSaveRequest.builder()
+//                .
+//                .build()
+//    }
+
+    @BeforeEach
+    public void deleteAll() {
+        userRepository.deleteAll();
+        postRepository.deleteAll();
+        commentRepository.deleteAll();
+    }
+
+
+    // TODO 카테고리 완료되면 Create test 마저 진행하겠다
+    @Test
+    public void savePostSuccess() throws Exception {
+        //given
+
+        //when
+        
+        //then
+    }
+
+    @Test
+    public void findPostByAuthorSuccess() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
 }
