69bcbcd866c922efe29428b50bbf374d47353ae8
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-12-17 15:50:36 +0900

    [ADD]nav바 height 설정

--- frontend/src/component/home/HomeStart.js
+++ frontend/src/component/home/HomeStart.js
@@ -1,8 +1,15 @@
 import React, {useState, useEffect, useMemo, useCallback} from 'react';
 // import styles from "../../styles/home/Home.module.css";
 import styles from "../../styles/home/HomeStart.module.css"
+import { useNavigate } from 'react-router-dom';
 
 const HomeStart = () => {
+    const navigate = useNavigate();
+
+
+    const onClickStart = () => {
+        navigate(`/login`)
+    }
     return (
         <section className={styles.start}>
             <div className={styles.startLeft}>CO끼리</div>
@@ -11,7 +18,7 @@ const HomeStart = () => {
                     중고 거래부터 동네 인증까지, 코끼리와 함께해요.<br/>
                     가볍고 따뜻한 코끼리를 만들어요.</div>
                 <div className={styles.startRight2}>
-                    <button>시작하기</button>
+                    <button onClick={onClickStart}>시작하기</button>
                     <button>내 물건 올리기</button>
                 </div>
             </div>
--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect, useMemo, useCallback} from 'react';
 const Login = () => {
     return (
         <>
-    <h1>HI</h1>
+
         </>
     );
 }
