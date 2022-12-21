db3a70a621b5089b1afdcfeb6afcfea0d9860ae3
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-12-22 00:38:08 +0900

    [REFACTOR]끼리끼리 redux 값 전달

--- frontend/src/routes/KiriKiriTrade.tsx
+++ frontend/src/routes/KiriKiriTrade.tsx
@@ -7,9 +7,15 @@ import myImage from "../img/cokkiriLogo.png"
 import TradeCategory from "../component/trade/TradeCategory";
 import PriceBox from "../component/trade/PriceBox";
 import PostContainer from "../component/trade/PostContainer";
+import {useDispatch, useSelector} from "react-redux";
+import {Rootstate} from "../index";
 
 
 const KiriKiriTrade = () => {
+
+    const store = useSelector((state:Rootstate) => state);
+    const dispatch = useDispatch();
+
     return (
         <div className={styles.mulmulTrade}>
             <div className={styles.mulmulTradeContent}>
@@ -20,7 +26,7 @@ const KiriKiriTrade = () => {
                     <TradeCategory/>
                     <PriceBox/>
                 </div>
-                <div className={styles.navPostOrWant}>도서끼리 교환해요</div>
+                <div className={styles.navPostOrWant}>{store.categoryReducer.category}끼리 교환해요</div>
                 <div className={styles.popularOrNewest}>
                     <div className={styles.pupularBtn}>인기도순</div>
                     <div className={styles.slash}>|</div>
