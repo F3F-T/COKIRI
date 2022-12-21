74ab5ff819182c98ba9ac973dc449fae7c6d3656
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-12-21 23:15:20 +0900

    [ADD]redux-toolkit 환경구축, 연습

---
+++ frontend/src/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
+import counterReducer from "./store/counterReducer";
+
+/**
+ * configureStore을 import해온다.
+ * store을 configureStore을 통해서 생성, reducer에는 만들 reducer을 객체형태로 저장한다.
+ * 객체의 값은 리듀서가 들어간다.
+ * counterReducer는 counterReducer.ts의 counterSlice.reducer이다 !!
+ *
+ * 이것을 Provider로 <App/>을 감싸면 App내의 모든 컴포넌트는 특정 훅을 통해 store의 내용을 자유롭게 사용할수 있다.
+ *
+ *
+ */
+
+
+//store/counterReducer에서 만든 reducer, action, state, 초기 설정값등을 reducer에 묶어주고, counter로 선언해준다
+export const store = configureStore({
+    reducer : {
+        counter : counterReducer
+    }
+})
+
+export type Rootstate = ReturnType<typeof store.getState>
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ frontend/src/store/counterReducer.ts
@@ -0,0 +1,40 @@
+/**
+ * 초기 값의 타입을 지정해주고,
+ * action에 PayloadAction 제네릭 타입 추가를 해준다.
+ *
+ */
+
+import {createSlice,PayloadAction} from "@reduxjs/toolkit";
+
+interface InitialState{
+    count : number
+}
+
+//initialState에서 결국 변수를 저장하고, 사용하는거구나
+
+const initialState: InitialState = {count : 0}
+
+//createSlice에선 action을 지정해준다. action 을 reducer라고 생각하면 되겠네.
+//접근을 할때는 name으로 선언한
+const counterSlice = createSlice({
+    name: "counter",
+    initialState : initialState,
+    reducers : {
+        increase(state){
+            state.count+=1
+        },
+        decrease(state){
+            state.count-=1
+        },
+        increaseByAmount(state, action : PayloadAction<number>){
+            state.count += action.payload
+        }
+    }
+})
+
+
+//위에서 선언해준 counterSlice의 reducer를 export해준다
+export default counterSlice.reducer;
+export const {increase,decrease,increaseByAmount} = counterSlice.actions;
+
+//여기선 알아서 api가 해주는구나, 대충 여기서 선언한 initial state, type, action을 보내주면 된다고 생각하면 되겠네
