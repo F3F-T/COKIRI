ebda3127736e652fee4ef3c1fcbde2a576cf65cd
miin-hyukkk <ham9893@naver.com>
2022-12-22 02:43:26 +0900

    [add] card 컴포넌트화(미완성)

--- frontend/src/component/trade/PostContainer.tsx
+++ frontend/src/component/trade/PostContainer.tsx
@@ -4,6 +4,8 @@ import {useNavigate} from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import myImage from "../../img/cokkiriLogo.png"
+import Card from "../tradeCard/Card"
+import Comments from "../comments/Comments";
 
 
 // ㅂㅅ
@@ -14,22 +16,20 @@ const PostContainer = () => {
 
         return (
         <div className={styles.postContainer}>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage} onClick={()=>navigate('/post')}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
-            <div className={styles.postItem}><img className={styles.postImage} src = {myImage}/></div>
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
+            <Card className={"forTrade"} postTitle={"스팸씨발아"} postContent={"스패애애ㅐㅇㅁ"} like={3} comment={5} category={"가구"} />
         </div>
     );
 }
---
+++ frontend/src/component/tradeCard/Card.tsx
@@ -0,0 +1,76 @@
+import React, {useState, useEffect, useMemo, useCallback} from 'react';
+import styles from "../../styles/card/cards.module.scss";
+import profileImg from "../../img/profileImg.png";
+import classNames from "classnames/bind";
+import myImage from "../../img/cokkiriLogo.png";
+import PostContainer from "../trade/PostContainer";
+
+
+
+//classNames로 styles를 bind해서 styles에 쉽게 접근하고 css 조건문을 쉽게 달수있게 돕는 API
+const ck = classNames.bind(styles)
+
+//물물교환에 쓸 css와 내 정보에 쓸 css를 구분하기 위해 사용  유니온 뭐시기였어 이게
+type cardTypes = "forTrade" | "forMypage"
+
+
+//props에서 받을 카드 속 컨탠츠들
+interface props{
+    className?: cardTypes; //옵셔널로 준 이유가 뭐라 그랬지
+    postTitle: string;
+    postContent: string;
+    like: number;
+    comment: number;
+    category : string; //나중에 enum사용해서 다시 해보던가 할듯 없는 카테고리 못들어오게 막아야지
+}
+
+const TradeCard = (props1:props)=>{
+    return(
+        <>
+            <div className={styles.postItem}>
+                <img className={styles.postImage} src = {myImage}/>
+                <p className={styles.postTitle}>{props1.postTitle}</p>
+                <p className={styles.postContent}>{props1.postContent}</p>
+                <div className={styles.detail}>
+                    <li>{props1.like}</li>
+                    <li>{props1.comment}</li>
+                    <li>{props1.category}</li>
+                </div>
+            </div>
+
+        </>
+    )
+}
+const MypageCard = (props1:props)=>{
+    return(
+        <>
+            <div className={styles.postItem}>
+                <img className={styles.postImage} src = {myImage}/>
+                <p className={styles.postTitle}>{props1.postTitle}</p>
+                <p className={styles.postContent}>{props1.postContent}</p>
+                <div className={styles.detail}>
+                    <li>{props1.like}</li>
+                    <li>{props1.comment}</li>
+                    <li>{props1.category}</li>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+const Card = (props1: props)=>{
+    return(
+        <>
+            {props1.className === "forTrade" && //이 &&이 뭔 연산자일까
+                <div className={ ck(props1.className)}>
+                    <TradeCard postTitle={props1.postTitle} postContent={props1.postContent} like={props1.like} comment={props1.comment} category={props1.category}/>
+                </div>}
+            {props1.className === "forMypage" &&
+                <div className={ ck(props1.className)}>
+                    <MypageCard postTitle={props1.postTitle} postContent={props1.postContent} like={props1.like} comment={props1.comment} category={props1.category}/>
+                </div>}
+        </>
+    )
+}
+export default Card;
