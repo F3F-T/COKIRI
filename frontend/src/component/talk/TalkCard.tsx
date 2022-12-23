86d2ba0999d78d3a6aa87d857bf1875c547463e2
miin-hyukkk <ham9893@naver.com>
2022-12-23 19:38:47 +0900

    [add] kokiriTalk css완성

--- frontend/src/component/talk/TalkCard.tsx
+++ frontend/src/component/talk/TalkCard.tsx
@@ -1,24 +1,21 @@
 import styles from "../../styles/talk/talkList.module.scss";
 import React from "react";
-import MyPage from "../MyPageSet";
+import TalkList from "./TalkList";
+import transfer from "../../img/transfer.png"
 
 
 interface keyProps {
     keys: number;
 }
+const TalkCard = (key: keyProps) => {
 
-
-const Message = (key: keyProps) => {
-
-    const message: string[] = ["보낸사람 : 이거 얼마", "받은 사람 : 삼천원",
-        "보낸사람 : 이거 얼마", "받은 사람 : 삼천원",
-        "보낸사람 : 이거 얼마", "받은 사람 : 삼천원",
-        "보낸사람 : 이거 얼마", "받은 사람 : 삼천원"
-    ]
-    //
 
     const objectMessage = [{
         keys: 1,
+        title: "21fw 쿠어 MTR 발마칸 코트M (멜란지토프)",
+        category: "의류",
+        tradeCategry: "가구",
+        url: "https://via.placeholder.com/600/771796",
         message: ["보낸 사람 : 이거 얼마", "받은 사람 : 삼천원",
             "보낸 사람 : 이거 얼마", "받은 사람 : 삼천원",
             "보낸 사람 : 이거 얼마", "받은 사람 : 삼천원",
@@ -32,6 +29,10 @@ const Message = (key: keyProps) => {
     },
         {
             keys: 2,
+            title: "운동화",
+            category: "의류",
+            tradeCategry: "의류",
+            url: "https://via.placeholder.com/600/92c952",
             message: ["보낸 사람 : 이거 얼마", "받은 사람 : 삼천원",
                 "보낸 사람 : 이거 얼마", "받은 사람 : 삼천원",
                 "보낸 사람 : 이거 얼마", "받은 사람 : 삼천원",
@@ -40,6 +41,10 @@ const Message = (key: keyProps) => {
         },
         {
             keys: 3,
+            title: "맥북",
+            category: "전자 제품",
+            tradeCategry: "의류",
+            url: "https://via.placeholder.com/600/771796",
             message: ["보낸 사람 : 이거 얼마", "받은 사람 : 삼천원",
                 "보낸 사람 : 이거 얼마", "받은 사람 : 삼천원",
                 "보낸 사람 : 이거 얼마", "받은 사람 : 삼천원",
@@ -47,34 +52,24 @@ const Message = (key: keyProps) => {
 
             ]
         },
-
-
     ]
-
-    console.log("키 값")
-    console.log(key.keys);
-
     const realKey = key.keys;
-    // axios.get( url/chatroom/realKey=1)
 
-
-
-    // <p className={styles.sendContent}>{singleMessage.slice(8,-1)}</p>
     return (
         <>
-            {objectMessage[realKey-1].message.map((singleMessage: string) => (
-                singleMessage.includes("보낸 사람") ?
-                    <div className={styles.send}>
-                        <div className={styles.sendTitle}>받은 쪽지</div>
-                        <p className={styles.sendContent}>{singleMessage.substring(7,20)}</p>
-                    </div> :
-                    <div className={styles.receive}>
-                        <div className={styles.receiveTitle}>보낸 쪽지</div>
-                        <p className={styles.receiveContent}>{singleMessage.slice(7,20)}</p>
+                    <img className={styles.cardImage} src={objectMessage[realKey-1].url }/>
+                    <div className={styles.productInfo}>
+                     <p className={styles.tradeState}>판매중</p>
+                     <p className={styles.cardTitle}> {objectMessage[realKey-1].title} </p>
+                    <div className={styles.tradeBox}>
+                    <p className={styles.cardCategory}> {objectMessage[realKey-1].category} </p>
+                    <img className={styles.tradeimoticon} src={transfer}/>
+                    <p className={styles.cardCategory}> {objectMessage[realKey-1].tradeCategry} </p>
                     </div>
-            ))}
+                    </div>
+
 
         </>
     )
 }
-export default Message;
+export default TalkCard;
