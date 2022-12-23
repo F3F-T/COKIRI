779afb258694b7d854bbef362470352545aa8036
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-12-23 19:27:55 +0900

    [ADD]cors 문제 해결중

--- frontend/src/routes/로그인 & 회원가입/SignUp.tsx
+++ frontend/src/routes/로그인 & 회원가입/SignUp.tsx
@@ -9,6 +9,9 @@ import TextInput from "../../component/common/TextInput";
 import Button from "../../component/common/Button";
 import Message from "../../component/로그인 & 회원가입/Message";
 
+import apiClient from "../../request/request"
+import {stringify} from "querystring";
+import axios from "axios";
 
 const SignUp = () => {
 
@@ -24,101 +27,124 @@ const SignUp = () => {
     const [passwordCheck, setpasswordCheck] = useState<boolean>(undefined);
 
     const [userInfo, setuserInfo] = useState<UserInfo>(null);
-
+    const [postResult, setPostResult] = useState(null);
     const navigate = useNavigate();
 
-    const signUpButtonClick = () => {
 
+    const formatResponse = (result) => {
+        //두번째 replace parameter는 filter같은 역할을 한다.
+        //세번째 parameter는 원하는 공백
+        JSON.stringify(result, null, 2);
 
-
-
-        navigate(`/signup/emailcheck`)
     }
 
-    const onChangeEmail = (e) => {
-        setuserInfo((prevState) => {
-            return {...prevState, email: e.target.value}
-        })
-    }
+    async function postSignUpData() {
+        try {
+            const res = await apiClient.post("/auth/signup", userInfo);
 
-    const onChangePassword = (e) => {
-        setuserInfo((prevState) => {
-            return {...prevState, password: e.target.value}
-        })
-    }
+            const result = {
+                status: res.status + "-" + res.statusText,
+                headers: res.headers,
+                data: res.data,
+            };
+            console.log(result);
+            // setPostResult(formatResponse(result));
+        } catch (err) {
+            console.log(err);
 
-    const onChangeCheckPassword = (e) => {
-        if (userInfo.password === e.target.value) {
-            setpasswordCheck(true);
-            console.log("OK")
-        } else {
-            setpasswordCheck(false);
-            console.log("비밀번호 일치하지 않음")
         }
     }
 
-    const onChangeName = (e) => {
-        setuserInfo((prevState) => {
-            return {...prevState, name: e.target.value}
-        })
-    }
 
-    const onChangeBirth = (e) => {
-        setuserInfo((prevState) => {
-            return {...prevState, birth: e.target.value}
-        })
-    }
+        const signUpButtonClick = () => {
+            console.log(userInfo);
+            postSignUpData();
+        }
 
-    const onChangeNickname = (e) => {
-        setuserInfo((prevState) => {
-            return {...prevState, nickname: e.target.value}
-        })
-    }
+        //입력완료하면 값이 state에 저장된다.
+        const onChangeEmail = (e) => {
+            setuserInfo((prevState) => {
+                return {...prevState, email: e.target.value}
+            })
+        }
 
-    const onChangePhoneNumber = (e) => {
-        setuserInfo((prevState) => {
-            return {...prevState, phonenumber: e.target.value}
-        })
-    }
+        const onChangePassword = (e) => {
+            setuserInfo((prevState) => {
+                return {...prevState, password: e.target.value}
+            })
+        }
 
-    return (
-        <div className={styles.signup}>
-            <div className={styles.signupHeader}>
-                <div className={styles.signup_1}>코끼리 ID 회원가입</div>
-                <div className={styles.signup_2}><br/></div>
-                <div className={styles.signup_3}>회원정보 입력</div>
+        const onChangeCheckPassword = (e) => {
+            if (userInfo.password === e.target.value) {
+                setpasswordCheck(true);
+                console.log("OK")
+            } else {
+                setpasswordCheck(false);
+                console.log("비밀번호 일치하지 않음")
+            }
+        }
+
+        const onChangeName = (e) => {
+            setuserInfo((prevState) => {
+                return {...prevState, name: e.target.value}
+            })
+        }
+
+        const onChangeBirth = (e) => {
+            setuserInfo((prevState) => {
+                return {...prevState, birth: e.target.value}
+            })
+        }
+
+        const onChangeNickname = (e) => {
+            setuserInfo((prevState) => {
+                return {...prevState, nickname: e.target.value}
+            })
+        }
+
+        const onChangePhoneNumber = (e) => {
+            setuserInfo((prevState) => {
+                return {...prevState, phonenumber: e.target.value}
+            })
+        }
+
+        return (
+            <div className={styles.signup}>
+                <div className={styles.signupHeader}>
+                    <div className={styles.signup_1}>코끼리 ID 회원가입</div>
+                    <div className={styles.signup_2}><br/></div>
+                    <div className={styles.signup_3}>회원정보 입력</div>
+
+                </div>
+                <div className={styles.userInfo}>
+
+                    <TextInput placeholder={"이메일"} onBlur={onChangeEmail}/>
+                    <TextInput placeholder={"비밀번호"} onBlur={onChangePassword}/>
+                    <TextInput placeholder={"비밀번호 확인"} onBlur={onChangeCheckPassword}/>
+                    {(passwordCheck === undefined && <Message passwordCheck={passwordCheck} content={""}/>)
+                        ||
+                        (passwordCheck ?
+                            <Message passwordCheck={passwordCheck} content={"✔ 비밀번호가 일치합니다"}/>
+                            :
+                            <Message passwordCheck={passwordCheck} content={"❌ 비밀번호가 일치하지 않습니다"}/>)}
+
+
+                    <section className={styles.userNameBirth}>
+                        <TextInput placeholder={"이름"} onBlur={onChangeName}/>
+                        <TextInput placeholder={"생일"} onBlur={onChangeBirth}/>
+                    </section>
+
+                    <TextInput placeholder={"닉네임"} onBlur={onChangeNickname}/>
+                    <TextInput placeholder={"전화번호"} onBlur={onChangePhoneNumber}/>
+                </div>
+
+                <div className={styles.btnPlace}>
+                    <Button content={"회원가입"} className={"black"} onClick={signUpButtonClick}/>
+                </div>
 
             </div>
-            <div className={styles.userInfo}>
+        );
+    }
 
-                <TextInput placeholder={"이메일"} onBlur={onChangeEmail}/>
-                <TextInput placeholder={"비밀번호"} onBlur={onChangePassword}/>
-                <TextInput placeholder={"비밀번호 확인"} onBlur={onChangeCheckPassword}/>
-                {(passwordCheck === undefined && <Message passwordCheck={passwordCheck} content={""}/>)
-                    ||
-                    (passwordCheck ?
-                        <Message passwordCheck={passwordCheck} content={"✔ 비밀번호가 일치합니다"}/>
-                        :
-                        <Message passwordCheck={passwordCheck} content={"❌ 비밀번호가 일치하지 않습니다"}/>)}
-
-
-                <section className={styles.userNameBirth}>
-                    <TextInput placeholder={"이름"} onBlur={onChangeName}/>
-                    <TextInput placeholder={"생일"} onBlur={onChangeBirth}/>
-                </section>
-
-                <TextInput placeholder={"닉네임"} onBlur={onChangeNickname}/>
-                <TextInput placeholder={"전화번호"} onBlur={onChangePhoneNumber}/>
-            </div>
-
-            <div className={styles.btnPlace}>
-                <Button content={"회원가입"} className={"black"} onClick={signUpButtonClick}/>
-            </div>
-
-        </div>
-
-
-    );
-}
 
 export default SignUp;
