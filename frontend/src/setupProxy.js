a8c5a81d1230497c9dbdbcf9ddb5e80a6d5843d2
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-12-28 23:26:01 +0900

    [ADD]setup proxy 오류

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/routes',
+        '/routes', //추후에 파일 경로 추가
         createProxyMiddleware({
             target: 'http://localhost:8080',
             changeOrigin: true,
