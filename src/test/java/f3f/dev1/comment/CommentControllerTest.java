2b5ad81f96b819fcf1982980aeba7e47f2c8c17c
Owen-Choi <oldstyle482@gmail.com>
2022-12-29 17:51:10 +0900

    [ADD] CommentController 테스트 완료

--- src/test/java/f3f/dev1/comment/CommentControllerTest.java
+++ src/test/java/f3f/dev1/comment/CommentControllerTest.java
@@ -10,6 +10,7 @@ import f3f.dev1.domain.model.Address;
 import f3f.dev1.domain.post.application.PostService;
 import f3f.dev1.domain.post.dto.PostDTO;
 import f3f.dev1.global.common.annotation.WithMockCustomUser;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,7 @@ import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -32,10 +34,14 @@ import java.util.List;
 import static f3f.dev1.domain.comment.dto.CommentDTO.*;
 import static f3f.dev1.domain.member.model.UserLoginType.EMAIL;
 import static f3f.dev1.domain.post.dto.PostDTO.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -144,6 +150,10 @@ public class CommentControllerTest {
                 .build();
     }
 
+    public UpdateCommentRequest createUpdateCommentRequest(Long commentId, Long authorId, Long postId, String content) {
+        return new UpdateCommentRequest(commentId, authorId, postId, content);
+    }
+
     @Test
     @DisplayName("댓글 생성 테스트")
     @WithMockCustomUser
@@ -155,7 +165,8 @@ public class CommentControllerTest {
         Long postId = postService.savePost(postSaveRequest);
 
         CreateCommentRequest commentRequest = createCommentRequest(member);
-        doReturn(null).when(commentService).createComment(any());
+        CommentInfoDto commentInfoDto = createCommentInfoDto(1L, postId, member.getId(), "새로 만든 댓글");
+        doReturn(commentInfoDto).when(commentService).createComment(any());
         postService.savePost(postSaveRequest);
 
         //when & then
@@ -163,7 +174,22 @@ public class CommentControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(commentRequest)))
                 .andDo(print())
-                .andExpect(status().isCreated());
+                .andExpect(jsonPath("$.content").value("새로 만든 댓글"))
+                .andExpect(status().isCreated())
+                .andDo(document("comment/create/success", requestFields(
+                        fieldWithPath("authorId").description("Id value of author"),
+                        fieldWithPath("postId").description("Id value of post"),
+                        fieldWithPath("depth").description("depth value of post"),
+                        fieldWithPath("content").description("content value of post"),
+                        fieldWithPath("parentCommentId").description("Id value of parent comment")
+                ), responseFields(
+                        fieldWithPath("id").description("Id value of comment"),
+                        fieldWithPath("postId").description("Id value of post"),
+                        fieldWithPath("memberId").description("Id value of member"),
+                        fieldWithPath("content").description("content of comment"),
+                        fieldWithPath("depth").description("depth value of comment"),
+                        fieldWithPath("parentCommentId").description("Id value of parent comment")
+                )));
 
     }
 
@@ -177,7 +203,6 @@ public class CommentControllerTest {
         Long postId = postService.savePost(postSaveRequest);
 
         // when
-        CreateCommentRequest commentRequest = createCommentRequest(member);
         List<CommentInfoDto> commentInfoDtoList = new ArrayList<>();
         CommentInfoDto commentInfoDto = createCommentInfoDto(1L, postId, member.getId(), "댓글1");
         commentInfoDtoList.add(commentInfoDto);
@@ -187,14 +212,24 @@ public class CommentControllerTest {
         //then
         mockMvc.perform(get("/post/{postId}/comments", 1L)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(commentRequest)))
+                .content(""))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0].content").value("댓글1"));
+                .andExpect(jsonPath("$.[0].content").value("댓글1"))
+                .andExpect(jsonPath("$.[0].memberId").value(member.getId()))
+                .andDo(document("comment/readCommentsByPostId/success", responseFields(
+                        fieldWithPath("[].id").description("Id value of comment"),
+                        fieldWithPath("[].postId").description("Id value of post"),
+                        fieldWithPath("[].memberId").description("Id value of member"),
+                        fieldWithPath("[].content").description("content of comment"),
+                        fieldWithPath("[].depth").description("depth value of comment"),
+                        fieldWithPath("[].parentCommentId").description("Id value of parent comment")
+                )));
     }
 
     @Test
     @DisplayName("댓글 조회 테스트 실패 - 없는 포스트에 대한 댓글 조회 요청")
+    // TODO /post/3/comments 랑 post/20/comments 랑 똑같은 body가 return된다... 왜지??
     public void findCommentsByPostIdTestForFail() throws Exception {
         //given
         doReturn(1L).when(postService).savePost(any());
@@ -203,21 +238,61 @@ public class CommentControllerTest {
         Long postId = postService.savePost(postSaveRequest);
 
         // when
-        CreateCommentRequest commentRequest = createCommentRequest(member);
         List<CommentInfoDto> commentInfoDtoList = new ArrayList<>();
         CommentInfoDto commentInfoDto = createCommentInfoDto(1L, postId, member.getId(), "댓글1");
         commentInfoDtoList.add(commentInfoDto);
-        doReturn(commentInfoDtoList).when(commentService).findCommentsByPostId(any());
+        // 20번 아이디를 가진 포스트는 존재하지 않는다고 가정.
+        doReturn(null).when(commentService).findCommentsByPostId(20L);
         postService.savePost(postSaveRequest);
 
         //then
-        mockMvc.perform(get("/post/{postId}/comments", 30L)
+        MvcResult mvcResult = mockMvc.perform(get("/post/{postId}/comments", 20L)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(commentRequest)))
+                .content(""))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0]").isEmpty());
-//                .andExpect(jsonPath());
+                .andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+        assertThat(content).isEqualTo("");
     }
 
+
+    @Test
+    @DisplayName("댓글 수정 테스트")
+    public void updateCommentTestForSuccess() throws Exception {
+        //given
+        doReturn(1L).when(postService).savePost(any());
+        Member member = createMember();
+        PostSaveRequest postSaveRequest = createPostSaveRequest(member, false);
+        Long postId = postService.savePost(postSaveRequest);
+
+        //when
+        CommentInfoDto commentInfoDto = createCommentInfoDto(1L, postId, member.getId(), "새로 만든 댓글");
+        doReturn(commentInfoDto).when(commentService).createComment(any());
+        UpdateCommentRequest updateCommentRequest = createUpdateCommentRequest(1L, postId, member.getId(), "수정한 댓글");
+        CommentInfoDto updatedCommentInfoDto = createCommentInfoDto(1L, postId, member.getId(), updateCommentRequest.getContent());
+        doReturn(updatedCommentInfoDto).when(commentService).updateComment(any());
+
+        //then
+        mockMvc.perform(patch("/post/{postId}/comments/{commentId}", 1L, 1L)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(updateCommentRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value(updatedCommentInfoDto.getContent()))
+                .andDo(document("comment/update/success", requestFields(
+                        fieldWithPath("id").description("id value of comment"),
+                        fieldWithPath("authorId").description("id value of author"),
+                        fieldWithPath("postId").description("id value of post"),
+                        fieldWithPath("content").description("updated content value of comment")
+                ), responseFields(
+                        fieldWithPath("id").description("Id value of comment"),
+                        fieldWithPath("postId").description("Id value of post"),
+                        fieldWithPath("memberId").description("Id value of member"),
+                        fieldWithPath("content").description("content of comment"),
+                        fieldWithPath("depth").description("depth value of comment"),
+                        fieldWithPath("parentCommentId").description("Id value of parent comment")
+                )));
+    }
 }
