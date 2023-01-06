17c4e11e92eb0a39d7e9fd7a48e65ba7ae55d780
Owen-Choi <oldstyle482@gmail.com>
2023-01-07 02:14:16 +0900

    [ADD] 게시글 단일 조회 응답 데이터 추가 중

--- src/main/java/f3f/dev1/domain/comment/dao/CommentRepository.java
+++ src/main/java/f3f/dev1/domain/comment/dao/CommentRepository.java
@@ -1,11 +1,16 @@
 package f3f.dev1.domain.comment.dao;
 
+import f3f.dev1.domain.comment.dto.CommentDTO;
 import f3f.dev1.domain.comment.model.Comment;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+
+import static f3f.dev1.domain.comment.dto.CommentDTO.*;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
@@ -18,4 +23,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     // user id랑 comment id를 함께 충족하는 댓글을 찾는다
     Optional<Comment> findByAuthorIdAndId(Long authorId, Long commentId);
     boolean existsByPostId(Long postId);
+
+    @Query("select new f3f.dev1.domain.comment.dto.CommentDTO.CommentInfoDto(c.id, c.post.id, c.author.id, c.content, c.depth, c.parent.id) " +
+            "from Comment c where c.post.id = :postId")
+    List<CommentInfoDto> findCommentInfoDtoByPostId(@Param("postId") Long postId);
 }
