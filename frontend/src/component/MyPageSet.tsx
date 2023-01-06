84a17377f636b6e15e2e4b0dd1f99936022bafa3
miin-hyukkk <ham9893@naver.com>
2023-01-07 01:32:00 +0900

    [add] mypage 미완2

--- frontend/src/component/MyPageSet.tsx
+++ frontend/src/component/MyPageSet.tsx
@@ -9,31 +9,142 @@ import profile from "../img/profile.jpeg"
 import PostContainer from "../component/trade/PostContainer";
 import axios from "axios";
 import Api from "../utils/api"
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {Rootstate} from "../index";
+import {Simulate} from "react-dom/test-utils";
+import input = Simulate.input;
+import {setUserNick} from "../store/userInfoReducer";
+import {userInfo} from "os";
+import TextInput from "./common/TextInput";
+import Message from "./로그인 & 회원가입/Message";
 
 // interface TextInputProps {
 //     init: string;
 // }
 
 const MyPage = () =>  {
-    const [tab1, setTab] = useState<string>('curr');
+    interface UserInfo {
+        userId:number;
+        newNickname: string;
+
+    }
+    type checkNicknameTypes = 'invalid' | 'valid' | 'duplicated'
+
+    interface ValidationCheck {
+        nicknameCheck: checkNicknameTypes;
+        nicknameCheckBoolean: boolean;
+    }
+
+    const store = useSelector((state:Rootstate) => state);
+    const dispatch = useDispatch();
     const navigate = useNavigate();
     const info = useSelector((state : Rootstate)=>{return state.userInfoReducer})
     console.log("닉네임입니다.", info.nickname)
+    const [userInfo, setuserInfo] = useState<UserInfo>(null);
+    const [validationCheck, setValidationCheck] = useState<ValidationCheck>(
+        {
+            nicknameCheck: undefined,
+            nicknameCheckBoolean: undefined,
+        }
+    );
+    const onChangeNickname = (e) => {
+        let inputNickname = e.target.value;
+        //이메일 유효성 검사를 통과했을때, (형식에 맞는 경우 true 리턴)
+        if (inputNickname.length > 0) {
+            //닉네임 중복체크 백엔드 통신
+            //string인 inputNickname을 json형태의 객체로 변환
+            let jsonObj = {"nickname": inputNickname};
+            //변환한 json 객체로 이메일 중복체크
+            CheckNickNameDuplicated(jsonObj);
+        } else //닉네임 유효성 검사 실패했을때
+        {
+            setValidationCheck((prevState) => {
+                return {...prevState, nicknameCheck: "invalid", nicknameCheckBoolean: false}
+            })
+        }
+    }
+    async function CheckNickNameDuplicated(nickname: object) {
+
+        try {
+            const res = await axios.post("http://localhost:8080/auth/check-nickname", nickname);
+            console.log("dd닉ㄴ아럼닏ㄴㄹ",res);
+            const result = res.data;
+            console.log("dd닉네임ㅇㄴ아럼닏ㄴㄹ",result);
+            const duplicated = result.exists
+
+            if (duplicated) //중복인 경우 -> true 반환
+            {
+                setValidationCheck((prevState) => {
+                    return {...prevState, nicknameCheck: "duplicated", nicknameCheckBoolean: false}
+                })
+            } else //중복이 아닌 경우 -> false 반환
+            {
+                setValidationCheck((prevState) => {
+                    return {...prevState, nicknameCheck: "valid", nicknameCheckBoolean: true}
+                })
+                setuserInfo((prevState) => {
+                    return {
+                        ...prevState, newNickname: nickname["nickname"]
+                    }
+                })
+
+            }
+
+        } catch (err) {
+            console.log(err);
+            alert('서버와 통신 실패');
+
+        }
+    }
+    async function nicknameChange() {
+        try {
+            const res = await Api.patch("/user/nickname", userInfo);
+
+            const result = {
+                status: res.status + "-" + res.statusText,
+                headers: res.headers,
+                data: res.data,
+            };
+            console.log(result);
+            console.log("바뀐 유저정보", userInfo);
+            dispatch(setUserNick(userInfo.newNickname));
+            console.log("바뀐 닉네임", info.nickname);
+
+
+        } catch (err) {
+            console.log(err);
+            alert('닉넴 변경 실패');
+
+        }
+    }
+
+
+
     async function readNickName(){
         try{
             const res = await  Api.get("/user")
-            console.log("유저정보",res.data.nickname)
-            alert("전송")
+            console.log("유저정보",res.data.id)
+            setuserInfo((prevState) => {
+                return {
+                    ...prevState, userId: res.data.id
+                }
+            })
+
         }
         catch (err){
             console.log(err);
             alert("실패")
         }
     }
+    const [value,setValue]=useState('');
 
-
+    const onChange = useCallback(e=>{
+        setValue((e.target.value))
+    },[])
+    console.log("value",value);
+    useEffect(()=>{
+        readNickName()
+    },[])
 
     return (
             <>
@@ -42,7 +153,22 @@ const MyPage = () =>  {
                     <img className={styles.Image} src={profile}/>
                 </div>
                 <div className={styles.userInfo}>
-                    <div className={styles.nickName} placeholder={"닉네임"}>{info.nickname}</div>
+                    <form className = "TodoInsert">
+                        <input className={styles.nickName} placeholder={info.nickname} value={value} onChange={onChange} />
+                    </form>
+                    <TextInput placeholder={info.nickname} onBlur={onChangeNickname}/>
+                    {(validationCheck.nicknameCheck === undefined &&
+                            <Message validCheck={validationCheck.nicknameCheckBoolean} content={""}/>)
+                        ||
+                        (validationCheck.nicknameCheck === "valid" &&
+                            <Message validCheck={validationCheck.nicknameCheckBoolean} content={"✔ 사용 가능한 닉네임입니다."}/>)
+                        ||
+                        (validationCheck.nicknameCheck === "invalid" &&
+                            <Message validCheck={validationCheck.nicknameCheckBoolean} content={"❌ 닉네임은 한글자 이상이어야합니다."}/>)
+                        ||
+                        (validationCheck.nicknameCheck === "duplicated" &&
+                            <Message validCheck={validationCheck.nicknameCheckBoolean} content={"❌ 이미 가입된 닉네임입니다."}/>)}
+                    <button className={styles.nickChangeBtn} onClick={nicknameChange}>변경</button>
                     <input className={styles.intro} placeholder={"한 줄 소개를 입력하세요."}></input>
                     <div className={styles.intro2}>
                         <div className={styles.i1}>
