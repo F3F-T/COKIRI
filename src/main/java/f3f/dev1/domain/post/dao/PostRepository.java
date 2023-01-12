2e4bebaca77c6a2b36d06b6c9ecd8e58394981aa
Owen-Choi <oldstyle482@gmail.com>
2023-01-13 04:11:16 +0900

    [DELETE] 기존 조회 로직 삭제, 테스트 코드 대량 삭제

--- src/main/java/f3f/dev1/domain/post/dao/PostRepository.java
+++ src/main/java/f3f/dev1/domain/post/dao/PostRepository.java
@@ -20,30 +20,11 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRep
     List<Post> findAll();
     boolean existsById(Long id);
     Optional<Post> findById(Long id);
-    List<Post> findByPrice(Long price);
     boolean existsByAuthorId(Long authorId);
     List<Post> findByAuthorId(Long authorId);
-    List<Post> findByPriceLessThanEqual(Long maxPrice);
     List<Post> findByPostTagsIn(List<PostTag> postTags);
-    List<Post> findByPriceGreaterThanEqual(Long minPrice);
-    List<Post> findByWishCategoryName(String wishCategoryName);
-    List<Post> findByPriceBetween(Long minPrice, Long maxPrice);
-    List<Post> findByProductCategoryName(String productCategoryName);
-    List<Post> findByWishCategoryNameAndPostTagsIn(String wishCategoryName, List<PostTag> postTags);
-    List<Post> findByProductCategoryNameAndPostTagsIn(String productCategoryName, List<PostTag> postTags);
-    List<Post> findByProductCategoryNameAndWishCategoryName(String productCategoryName, String wishCategoryName);
-    List<Post> findByProductCategoryNameAndWishCategoryNameAndPostTagsIn(String productCategoryName, String wishCategoryName, List<PostTag> postTags);
     void deleteById(Long id);
-
     Page<Post> findAll(Pageable pageable);
-    Page<Post> findByPostTagsIn(List<PostTag> postTags, Pageable pageable);
 
-    @Query(nativeQuery = true,value = "select p.post_id , p.title , t.name\n" +
-            "from post p \n" +
-            "join post_tag pt on pt.post_id = p.post_id\n" +
-            "join tag t on t.tag_id = pt.tag_id  \n" +
-            "where t.name in (${tagNameList}) and count(p.post_id) = 3\n" +
-            "group by p.post_id , t.name\n")
-    List<Post> findByPostInTagName(String tagNameList);
 
 }
