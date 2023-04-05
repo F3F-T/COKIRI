7118fbb4e5de6e37eabda65ac65cf9ca17e9dbd8
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-04-05 19:13:01 +0900

    [ADD] cors 관련 수정

--- src/main/java/f3f/dev1/global/config/WebConfig.java
+++ src/main/java/f3f/dev1/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3000);
     }
