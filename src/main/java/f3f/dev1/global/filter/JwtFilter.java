7a06958915ea1430dcd1b3aa2e7dc710355ef661
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-06-23 20:52:08 +0900

    [ADD] request uri, memberId 확인을 위한 로그 추가

--- src/main/java/f3f/dev1/global/filter/JwtFilter.java
+++ src/main/java/f3f/dev1/global/filter/JwtFilter.java
@@ -3,10 +3,12 @@ package f3f.dev1.global.filter;
 import f3f.dev1.domain.member.exception.NotAuthorizedException;
 import f3f.dev1.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,6 +22,7 @@ import static f3f.dev1.global.common.constants.JwtConstants.AUTHORIZATION_HEADER
 import static f3f.dev1.global.common.constants.JwtConstants.BEARER_PREFIX;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -27,6 +30,9 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        log.info(request.getMethod()+" "+ request.getRequestURI());
+
         // 1. request Header에서 토큰 꺼냄
         String jwt = resolveToken(request);
         // 2. validateToken으로 유효성 검사
@@ -39,6 +45,8 @@ public class JwtFilter extends OncePerRequestFilter {
             if (StringUtils.hasText(jwt) && hasKey && jwtTokenProvider.validateToken(jwt)) {
                 Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+                User user = (User) authentication.getPrincipal();
+                log.info("memberId : " + user.getUsername());
 
             }
         }
