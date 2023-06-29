6eb3849eb953fab4f0dc54f9fbb2c6eced05fccb
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-06-29 15:48:12 +0900

    [ADD] 로그아웃 수정

--- src/main/java/f3f/dev1/global/jwt/CustomLogoutSuccessHandler.java
+++ src/main/java/f3f/dev1/global/jwt/CustomLogoutSuccessHandler.java
@@ -1,5 +1,7 @@
 package f3f.dev1.global.jwt;
 
+import f3f.dev1.domain.member.exception.UserNotFoundByEmailException;
+import f3f.dev1.domain.member.exception.UserNotFoundException;
 import f3f.dev1.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +27,7 @@ import java.io.IOException;
 public class CustomLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler implements LogoutSuccessHandler {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final JwtTokenProvider jwtTokenProvider;
     @Override
     @Transactional
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -36,10 +39,15 @@ public class CustomLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler im
 
         String token = request.getHeader("Authorization").split(" ")[1];
         log.info("token = " + token);
+        Authentication auth = jwtTokenProvider.getAuthentication(token);
+        User principal = (User) auth.getPrincipal();
+
 
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-        User user = (User) authentication.getPrincipal();
-        valueOperations.getAndDelete(user.getUsername());
+        if (valueOperations.get(principal.getUsername()) == null) {
+            throw new UserNotFoundException();
+        }
+        valueOperations.getAndDelete(principal.getUsername());
 
         log.info(request.getRequestURI());
         super.onLogoutSuccess(request, response, authentication);
